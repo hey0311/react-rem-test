3315c33364869bf8a541d92782e9cb0915feec42
hey0311 <hey0311@qq.com>
2019-08-20 14:14:06 +0800

    add rem

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './App3.scss'
+class App extends React.Component{
+    componentDidMount() {
+        // window.onresize = setHtmlFontSize
+        // function setHtmlFontSize () {
+        //     const htmlWidth = document.documentElement.clientWidth || document.body.clientWidth
+        //     const htmlDom = document.getElementsByTagName('html')[0]
+        //     htmlDom.style.fontSize = htmlWidth / 10 + 'px'
+        // }
+        // setHtmlFontSize()
+    }
 
-function App() {
-  return (
-    <div className="App">
-      <div className='bar'></div>
-    </div>
-  );
+    render(){
+        return (
+            <div className="App">
+                <div className='bar1'></div>
+                <div className='pie1'></div>
+            </div>
+        );
+    }
 }
 
 export default App;
